e8a49619ca3566ae18995ed30c9fb0c4e5030fe7
Ehab Elsayed <ehabsmh3@gmail.com>
2023-11-12 23:05:59 +0200

    modules imported on console.py

--- console.py
+++ console.py
@@ -2,6 +2,13 @@
 """Entry point of the command interpreter"""
 import cmd
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 import shlex
 
 
